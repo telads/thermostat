d967aed585060301b69787cf249eecb20b9ccdef
AnastasisGi <anastasiosgks@gmail.com>
2020-11-03 19:00:38 +0000

    Updated1

--- jasmine-standalone-3/lib/jasmine-3.6.0/thermostat.js
+++ jasmine-standalone-3/lib/jasmine-3.6.0/thermostat.js
@@ -1,35 +1,78 @@
-// use 'strict'
+'use strict';
 
 class Thermostat{
   constructor(temperature){
     this.temperature=20
     this.MINIMUM_TEMPERATURE = 10;
-    this.isPowerSavingModeOn=true;
+    this.powerSavingMode=true;
+    this.MAX_PSM_ON=25;
+    this.MAX_PSM_OFF=32;
   }
+
+
+  getCurrentTemperature() {
+      return this.temperature;
+    }
+
+
+    isPowerSavingModeOn(){
+      return this.powerSavingMode===true;
+    }
+
+
+    isMaximumTemperature() { 
+      if (this.isPowerSavingModeOn() === false) {
+        return  this.temperature===this.MAX_PSM_OFF;
+      }
+      return this.temperature === this.MAX_PSM_ON;
+    }
+
 
 
  increase () {
+   if (isMaximumTemperature()) {
+return;
+   }
   this.temperature+=1
-
 }
 
-   decrease () {
-      if (this.isMinimumTemperature ()) {
-        return;
-      }
-      this.temperature-=1
+
+  decrease () {
+    if(this.isMinimumTemperature()){
+      return;
+    }
+    this.temperature-=1
   }
+
+
+
+
+
+
+
+  //  decrease () {
+  //     if (this.isMinimumTemperature ()) {
+  //       return;
+  //     }
+  //     this.temperature-=1
+  // }
 
   isMinimumTemperature () {
     return this.MINIMUM_TEMPERATURE === this.temperature;
   }
 
   changePowerSavingModeToOff(){
-    return this.isPowerSavingModeOn=false;
+    return this.powerSavingMode=false;
   }
 
 changePowerSavingModeToOn(){
   return this.isPowerSavingModeOn=true;
 }
+
+
+
+
+
+
 
 }
--- jasmine-standalone-3/spec/thermostat_spec.js
+++ jasmine-standalone-3/spec/thermostat_spec.js
@@ -1,56 +1,63 @@
 // # Thermostat starts at 20 degrees
 
 describe("A thermostat", function(){
+  var thermostat;
+  beforeEach(function(){
+    thermostat=new Thermostat();
+  })
+
 
   it("Has a starting temperature of 20 degrees", function(){
-    let thermostat = new Thermostat ();
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
 
   it('Can increase the temperature with an up function', function () {
-    let thermostat = new Thermostat ();
     thermostat.increase()
     thermostat.increase()
-    expect(thermostat.temperature).toEqual(22);
+    expect(thermostat.getCurrentTemperature()).toEqual(22);
 
   });
 
   it('Can decrease the temparature by one ', function () {
-    let thermostat = new Thermostat();
     thermostat.increase()
     thermostat.increase()
     thermostat.decrease()
-    expect(thermostat.temperature).toEqual(21);
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
     });
 
-
+    //
     it('Has a minimum temperature of 10 degrees', function () {
-      let thermostat = new Thermostat();
-
-      for (var i = 1; i < 11; i++) {
+      for (var i = 1; i < 20; i++) {
         thermostat.decrease();
       }
-    expect(thermostat.temperature).toEqual(10);
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
     });
 
     it('Has a powersaving mode on by default', function (){
-      let thermostat = new Thermostat();
-
-      expect(thermostat.isPowerSavingModeOn).toEqual(true)
+      expect(thermostat.isPowerSavingModeOn()).toBe(true)
     })
-
-    it('Can change the powersaving mode from on to off', function (){
-      let thermostat = new Thermostat();
+    //
+    //
+    it('Can change the powersaving mode to off and on again ', function (){
       thermostat.changePowerSavingModeToOff();
-      expect(thermostat.isPowerSavingModeOn).toBe(false)
+      expect(thermostat.isPowerSavingModeOn()).toBe(false)
+      thermostat.changePowerSavingModeToOn();
+      expect(thermostat.isPowerSavingModeOn).toBe(true)
     })
 
-        it('Can change the powersaving mode from off to on ', function (){
-          let thermostat = new Thermostat();
-          thermostat.changePowerSavingModeToOn();
-          expect(thermostat.isPowerSavingModeOn).toBe(true)
-        })
+    describe('When the powersaving is on', function () {
+      it('it has a max temp of 25', function () {
+        for (var i = 0; i < 10; i++) {
+          thermostat.increase()
+        }
+        expect(thermostat.getCurrentTemperature()).toEqual(25);
+      });
+    });
+
+
+
+
 
 
 });
