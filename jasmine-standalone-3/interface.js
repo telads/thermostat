619337e20f9cb7a5c934c32ddaac145c0c2d8c97
Sabiha Teladia <s.telads@gmail.com>
2020-11-09 21:13:24 +0000

    weather api

--- jasmine-standalone-3/interface.js
+++ jasmine-standalone-3/interface.js
@@ -33,4 +33,18 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.getCurrentTemperature());
     $('#temperature').attr('class', thermostat.energyUse());
   };
+
+  // $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  //   $('#current-temperature').text(data.main.temp);
+  // })
+
+  
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+})
+
 });
